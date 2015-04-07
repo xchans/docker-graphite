723459f629c91a164b1935aa099be6ad300918fc
이찬희 <chlee@ichanhuiui-MacBook-Pro.local>
2015-04-07 18:18:16 +0900

    no message

--- grafana/setup_config.sh
+++ grafana/setup_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i -e "s/\(GRAPHITE_HOST\).*$/\1 = ${GRAPHITE_HOST}/g" /app/config.js
-sed -i -e "s/\(GRAPHITE_PORT\).*$/\1 = ${GRAPHITE_PORT}/g" /app/config.js
+sed -i -e "s/GRAPHITE_HOST/${GRAPHITE_HOST}/g" /app/config.js
+sed -i -e "s/GRAPHITE_PORT/${GRAPHITE_PORT}/g" /app/config.js
